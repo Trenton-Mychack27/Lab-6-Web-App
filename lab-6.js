8e6637d4622ca592d10660ca7b6f90a0a9f4cd01
Trenton-Mychack27 <tj27mych@siena.edu>
2025-03-19 16:10:26 -0400

    Added javascript edits for steps 3 and 4 from notion

--- lab-6.js
+++ lab-6.js
@@ -1,10 +1,13 @@
 function validateEmail() {
     let isValid = false;
-const emailAddress = // Get the emailAddress's input value
+const emailAddress = document.getElementById("emailAddress");// Get the emailAddress's input value
 //set the textContent of emailError as empty string.
+const emailError = document.getElementById("emailError");
+emailError.textContent = "";
 
-if (emailAddress === "" || !emailAddress.includes("@")) {
+if (emailAddress === "" || !emailAddress.includes("@siena")) {
 //set the content in emailError to "Please enter a valid email address";
+emailError.textContent = "Please enter a valid email address";
 } else {
 isValid = true;
 console.log("Email is valid: ", emailAddress);
@@ -13,7 +16,6 @@ console.log("Email is valid: ", emailAddress);
 
 if(isValid){
 // This submitRequest is a custom function that will be called when everything is valid
-    
-submitRequest();
+    submitRequest();
 }
 }
